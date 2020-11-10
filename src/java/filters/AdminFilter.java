3973329808257d844610f373a8873ccb6d1b2fa4
831690 <831690@SAIT224319IT.ACDM.DS.SAIT.CA>
2020-11-10 11:34:40 -0700

    Created Admin Filter, mapped to Admin Servlet, created logic

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,55 @@
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AdminFilter implements Filter {
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        String email = (String) session.getAttribute("email");
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        if (!email.equals("cprg352+admin@gmail.com")) {
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+        chain.doFilter(request, response);
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
+
