f7efeb9f7331188b3492c2ea8312f24a8f3e0fd9
831690 <831690@SAIT224319IT.ACDM.DS.SAIT.CA>
2020-11-10 11:05:02 -0700

    Merge origin/master

--- src/java/filters/AuthenticationFilter.java
+++ src/java/filters/AuthenticationFilter.java
@@ -15,37 +15,43 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class AuthenticationFilter implements Filter {
-    
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
 
-            // code that is executed before the servlet
-            HttpServletRequest httpRequest = (HttpServletRequest)request;
-            HttpSession session = httpRequest.getSession();
-            String email = (String)session.getAttribute("email");
-            
-            if (email == null) {
-                HttpServletResponse httpResponse = (HttpServletResponse)response;
-                httpResponse.sendRedirect("login");
-                return;
-            }
-            
-            chain.doFilter(request, response); // execute the servlet
-            
-            // code that is executed after the servlet
-            
+        // code that is executed before servlet
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        String email = (String) session.getAttribute("email");
         
+        if (email == null) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("login");
+            return;
+        }
+        
+        chain.doFilter(request, response);
+
+
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        
     }
 
     @Override
     public void destroy() {
-       
     }
+
 }
